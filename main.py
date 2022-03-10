fdc13509046432b0b7ecb8e0a6e70136af48b39a
unknown <b.kuba.13@gmail.com>
2022-03-10 17:06:16 +0100

    add correct location of threshold images

--- main.py
+++ main.py
@@ -6,24 +6,18 @@ import math
 import numpy as np
 import os
 from PIL import Image
+
+# !!! Useful code !!!
+
+# time measure !
+# import timeit
+# t = timeit.Timer("your_way()", "from __main__ import your_way")
+# print t.timeit(number=1)
+
+# print all result in console
 # np.set_printoptions(threshold=sys.maxsize)
 
-
-def slow_threshold_image(image):
-    number_of_panels, number_of_rows, number_of_columns, rgb = image.pixel_array.shape
-    for panel in range(number_of_panels):
-        print(f"Changing panel no. {panel}")
-        for row in range(number_of_rows):
-            if row % 50 == 0:
-                print(f"Row no: {row}")
-            for col in range(number_of_columns):
-                if image.pixel_array[panel, row, col, 0] >= 120 and image.pixel_array[panel, row, col, 1] <= 105:
-                    image.pixel_array[panel, row, col] = 255
-                else:
-                    image.pixel_array[panel, row, col] = 0
-        plt.axis("off")
-        plt.imshow(image.pixel_array[panel, :, 100:].astype(np.uint8))
-        plt.savefig(f"{panel}.png")
+####
 
 def fast_threshold(image):
     R = [(120, 255), (0, 105), (0, 255)]
@@ -36,13 +30,13 @@ def fast_threshold(image):
         image[panel][valid_range] = [255, 255, 255]
         image[panel][np.logical_not(valid_range)] = [0, 0, 0]
 
-        outim = Image.fromarray(image[panel])
-        outim.save(f"fast_{panel}.jpg")
+        outim = Image.fromarray(image[panel, :, 100:])
+        outim.save(fr".\images\fast_threshold\fast_{panel}.png")
+
 
 filepath = r"C:\Users\Jakub Lechowski\Desktop\PracaLicencjacka\Skrypty\ImageViewer\bazaDanychDCM\zdj.dcm"
 dcm_image = pydicom.dcmread(filepath)
 
-#slow_threshold_image(dcm_image)
 fast_threshold(dcm_image.pixel_array)
 
 
