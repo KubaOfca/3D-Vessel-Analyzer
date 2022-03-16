156e3a0121a36f5eabfa94abaf5f1a6e3810a07d
Jakub Baran <b.kuba.13@gmail.com>
2022-03-16 20:19:02 +0100

    clean up code

--- main.py
+++ main.py
@@ -1,51 +1,75 @@
 import matplotlib.pyplot as plt
 import pydicom
-import sys
-import cv2
-import math
 import numpy as np
-import os
 from PIL import Image
+from skimage.morphology import skeletonize_3d
+
 # np.set_printoptions(threshold=sys.maxsize)
 
 
-def slow_threshold_image(image):
-    number_of_panels, number_of_rows, number_of_columns, rgb = image.pixel_array.shape
-    for panel in range(number_of_panels):
-        print(f"Changing panel no. {panel}")
-        for row in range(number_of_rows):
-            if row % 50 == 0:
-                print(f"Row no: {row}")
-            for col in range(number_of_columns):
-                if image.pixel_array[panel, row, col, 0] >= 120 and image.pixel_array[panel, row, col, 1] <= 105:
-                    image.pixel_array[panel, row, col] = 255
-                else:
-                    image.pixel_array[panel, row, col] = 0
-        plt.axis("off")
-        plt.imshow(image.pixel_array[panel, :, 100:].astype(np.uint8))
-        plt.savefig(f"{panel}.png")
-
 def fast_threshold(image):
-    R = [(120, 255), (0, 105), (0, 255)]
-    for panel in range(39):
-        red_range = np.logical_and(R[0][0] < image[panel, :, :, 0], image[panel, :, :, 0] < R[0][1])
-        green_range = np.logical_and(R[1][0] < image[panel, :, :, 1], image[panel, :, :, 1] < R[1][1])
-        blue_range = np.logical_and(R[2][0] < image[panel, :, :, 2], image[panel, :, :, 2] < R[2][1])
-        valid_range = np.logical_and(red_range, green_range, blue_range)
+    RGB_threshold = [(120, 255), (0, 105), (0, 255)]
 
-        image[panel][valid_range] = [255, 255, 255]
-        image[panel][np.logical_not(valid_range)] = [0, 0, 0]
+    red_range = np.logical_and(RGB_threshold[0][0] < image[:, :, :, 0], image[:, :, :, 0] < RGB_threshold[0][1])
+    green_range = np.logical_and(RGB_threshold[1][0] < image[:, :, :, 1], image[:, :, :, 1] < RGB_threshold[1][1])
+    blue_range = np.logical_and(RGB_threshold[2][0] < image[:, :, :, 2], image[:, :, :, 2] < RGB_threshold[2][1])
+    valid_range = np.logical_and(red_range, green_range, blue_range)
 
-        outim = Image.fromarray(image[panel])
-        outim.save(f"fast_{panel}.jpg")
+    image[valid_range] = [255, 255, 255]
+    image[np.logical_not(valid_range)] = [0, 0, 0]
 
-filepath = r"C:\Users\Jakub Lechowski\Desktop\PracaLicencjacka\Skrypty\ImageViewer\bazaDanychDCM\zdj.dcm"
+    binary = np.logical_and(200 < image[:, :, :, 0], 255 >= image[:, :, :, 0])
+
+    image[binary] = [1, 1, 1]
+    image[np.logical_not(binary)] = [0, 0, 0]
+
+    return image
+
+def reduce_RGB_to_Binary(image):
+    image = np.delete(image, np.s_[1:3], 3).squeeze()
+    return image
+
+def plot_3D_from_3D_binary_image(image, save_to_file_from_diff_view=False):
+    """
+    Plot only values of pixel == 1
+    point size == 0.3
+    :param image: 3D ndarray
+    :return: None
+    """
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    z, x, y = image.nonzero()
+    ax.scatter(y, z, x, c="black", alpha=1, s=0.3)
+    if(save_to_file_from_diff_view):
+        for angle in range(0,360,20):
+            ax.view_init(elev=50., azim=angle)
+            plt.savefig(f"angle{angle}.png")
+    plt.show()
+
+def contour(image):
+    row, col, rgb = image.shape
+
+    for i in range(row):
+        for j in range(col):
+            if image[i, j, 0] == 255:
+                if image[i + 1, j, 0] != 255 or image[i - 1, j, 0] != 255 or image[i, j + 1, 0] != 255 or image[i, j - 1, 0] \
+                        != 255:
+                    image[i, j] = [255, 0, 0]
+
+    outim = Image.fromarray(image)
+    outim.save(f"contour.png")
+
+filepath = r"C:\Users\bkuba\Downloads\PracaLicencjacka-20220312T183334Z-002\PracaLicencjacka\Dane\dane_07.03.2022\Metformin_262p1_Power.dcm"
 dcm_image = pydicom.dcmread(filepath)
-
-#slow_threshold_image(dcm_image)
-fast_threshold(dcm_image.pixel_array)
-
-
-
+print("Threshold..")
+data = fast_threshold(dcm_image.pixel_array)
+print("Into Binary..")
+data = reduce_RGB_to_Binary(data)
+print("Skeletonize...")
+data = skeletonize_3d(data)
+print("Plotting...")
+plot_3D_from_3D_binary_image(data)
 
 
