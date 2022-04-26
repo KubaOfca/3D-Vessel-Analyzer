54dc54063f0b07cea13a8cb4d6c5014c7918eb85
unknown <b.kuba.13@gmail.com>
2022-04-26 12:09:20 +0200

    improve gui progress bar

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -14,8 +14,6 @@ import threading
 import os
 import math
 
-# Useful commends
-# np.set_printoptions(threshold=sys.maxsize)
 
 # TODO: 1 pomiary kretosci, modyfikacja szkieletonizacji w celu zbadania grubosci ???
 # TODO: przeliczenie na centymetry
@@ -29,6 +27,19 @@ stop_analyze = False
 def stop():
     global stop_analyze
     stop_analyze = True
+    progress_bar_label.configure(text="Stopping the analysis process. Please wait...")
+    progress_bar.configure(bootstyle="warning")
+    app.update_idletasks()
+
+
+def reset_app():
+    progress_bar.pack_forget()
+    progress_bar_label.pack_forget()
+    progress_bar_label.configure(text="File 0/0")
+    progress_bar.configure(value=0)
+    progress_bar.configure(bootstyle="success")
+    stop_analyze_button["state"] = "disabled"
+    app.update_idletasks()
 
 
 def check_before_analyze():
@@ -55,8 +66,8 @@ def check_before_analyze():
 
 
 def analyze_vessels(folder, list_of_dcm_files, number_of_dcm_files):
-    stop_analyze_button["state"] = "normal"
     global stop_analyze
+    stop_analyze_button["state"] = "normal"  # make stop button able to click
     result_dict = {
         "FileName": [],
         "Vessel-to-volume ratio [%]": [],
@@ -69,75 +80,57 @@ def analyze_vessels(folder, list_of_dcm_files, number_of_dcm_files):
         "Exec time": [],
     }
 
+    # progress bar control
+    progress_bar.pack(padx=20, pady=20)
+    progress_bar_label.pack(padx=20, pady=20)
     file_ctr = 0
-
     step_of_progress_bar = 100 / number_of_dcm_files
 
     for filename in list_of_dcm_files:
+        # if user click stop button
         if stop_analyze:
-            progress_bar_label.configure(text="File 0/0")
-            progress_bar.configure(value=0)
+            reset_app()
             messagebox.showinfo(
                 title="Info",
                 message="Analysis stopped"
             )
             stop_analyze = False
-            stop_analyze_button["state"] = "disabled"
-            app.update_idletasks()
             return
+
         file_full_path = os.path.join(folder, filename)
         if os.path.isfile(file_full_path):
             s_time = time.time()
 
+            # load file
             result_dict["FileName"].append(filename)
             dcm_image = pydicom.dcmread(file_full_path)
             dcm_as_array = dcm_image.pixel_array[:, ROI[1][0]:ROI[1][1], ROI[0][0]:ROI[0][1]]
+            # threshold and make image binary
             iman.threshold(dcm_as_array)
             dcm_as_array = iman.make_RGB_image_binary(dcm_as_array)
-            result_dict["Vessel-to-volume ratio [%]"].append(round(iman.vr(dcm_as_array), 2))
-            data_skeleton = skeletonize(dcm_as_array, method='lee')
-            # test_array = np.zeros((3, 5, 11), dtype=np.uint8)
-            # test_array[1, 3, 0] = 1
-            # test_array[1, 3, 1] = 1
-            # test_array[1, 3, 2] = 1
-            # test_array[1, 3, 3] = 1
-            # test_array[1, 3, 4] = 1
-            # test_array[1, 3, 5] = 1
-            # test_array[1, 3, 6] = 1
-            # test_array[1, 3, 7] = 1
-            # test_array[1, 3, 8] = 1
-            # test_array[1, 3, 9] = 1
-            # test_array[1, 1, 9] = 1
-            # test_array[1, 1, 8] = 1
-            # test_array[1, 1, 7] = 1
-            # test_array[1, 1, 6] = 1
-            # test_array[1, 1, 5] = 1
-            # test_array[1, 1, 4] = 1
-            # test_array[1, 1, 3] = 1
-            # test_array[1, 1, 2] = 1
-            # test_array[1, 2, 2] = 1
-            # test_array[1, 2, 10] = 1
-            # test_array[1, 2, 6] = 1
-            # test_array = skeletonize(test_array, method='lee')
-            # print(test_array)
+            # make skeleton from data
+            skeleton = skeletonize(dcm_as_array, method='lee')
+            # analyze and extract features
             trees, lv_feature, nb_feature, nc_feature, dm_feature, cp_feature, \
-                = form_array_of_skeleton_make_spanning_trees(data_skeleton)
+                = form_array_of_skeleton_make_spanning_trees(skeleton)
+            # save result to dict
             nt_feature = len(trees)
+            result_dict["Vessel-to-volume ratio [%]"].append(round(iman.vr(dcm_as_array), 2))
             result_dict["length of vessels"].append(round(lv_feature * PIXEL_SIZE, 2))
             result_dict["number of branching"].append(nb_feature)
             result_dict["number of cycles"].append(nc_feature)
             result_dict["number of vascular trees"].append(nt_feature)
             result_dict["distance metric"].append(round(dm_feature, 2))
             result_dict["soam"].append(round(cp_feature / lv_feature, 2))
-
             time_exec = round(time.time() - s_time, 2)
-            file_ctr += 1
-            print(f'[Exec Time] {time_exec}, Image: {file_ctr}')
             result_dict["Exec time"].append(time_exec)
+            # update progress bar
+            file_ctr += 1
             progress_bar.step(step_of_progress_bar)
             progress_bar_label.configure(text=f"File {file_ctr}/{number_of_dcm_files}")
             app.update_idletasks()
 
+    # save result to excel or csv file.
     df = pd.DataFrame(data=result_dict)
     result_file_name = datetime.now().strftime("%d.%m.%Y_%H.%M.%S")
     try:
@@ -162,17 +155,12 @@ def analyze_vessels(folder, list_of_dcm_files, number_of_dcm_files):
                     title="Error in saving to csv",
                     message=f'{error_text}\nResult won\'t be save'
                 )
-                stop_analyze_button["state"] = "disabled"
-                progress_bar_label.configure(text="File 0/0")
-                progress_bar.configure(value=0)
-                app.update_idletasks()
+                reset_app()
                 return
 
-    stop_analyze_button["state"] = "disabled"
-    progress_bar_label.configure(text="File 0/0")
-    progress_bar.configure(value=0)
-    app.update_idletasks()
-
+    # reset app to init state in order to be ready to next analyze
+    reset_app()
+    # ask to open a directory with result
     answer = messagebox.askyesno(
         title="Analysis complete!",
         message="Do you want to open directory with result file",
@@ -181,8 +169,19 @@ def analyze_vessels(folder, list_of_dcm_files, number_of_dcm_files):
         os.startfile(folder)
 
 
-# GUI
+# Main
+
+# GUI management
+WIDTH = 650
+HEIGHT = 450
+
 app = tk.Window(themename="darkly")
+# set window position
+app.minsize(WIDTH, HEIGHT)
+x = (app.winfo_screenwidth()/2) - (WIDTH/2)
+y = (app.winfo_screenheight()/2) - (HEIGHT/2)
+app.geometry('%dx%d+%d+%d' % (WIDTH, HEIGHT, x, y))
+
 app.title("Vessels Analyzer")
 title = tk.Label(
     app,
@@ -223,7 +222,7 @@ stop_analyze_button["state"] = "disabled"
 
 progress_bar = tk.Progressbar(
     app,
-    bootstyle=tk.SUCCESS,
+    bootstyle="success",
     length=100
 )
 
@@ -239,7 +238,5 @@ browse_dataset_button.grid(row=0, column=1, padx=20, pady=20)
 button_frame.pack(padx=20, pady=20)
 analyze_button.grid(row=0, column=0, padx=20, pady=5, sticky=tk.EW)
 stop_analyze_button.grid(row=1, column=0, padx=20, pady=5, sticky=tk.EW)
-progress_bar.pack(padx=20, pady=20)
-progress_bar_label.pack(padx=20, pady=20)
 
 app.mainloop()
